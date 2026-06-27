aa03e157872179795eb2d151d5659606ad624e87
Aditi Lande <aditilande02@gmail.com>
2026-06-27 18:10:56 +0530

    Solve/Note: all_products.sql

---
+++ solutions/leetcode/easy/all_products.sql
@@ -0,0 +1,8 @@
+-- Platform: LEETCODE
+-- Topic/Problem: ALL Products
+-- Difficulty/Category: Easy
+-- Link/Reference: https://www.codechef.com/practice/course/sql-case-studies-topic-wise/SQLBP01/problems/SQLPBP11
+-- Date Solved/Logged: 2026-06-27
+
+SELECT *
+FROM Products
